f4c541537faa55b9b89cb6e76d4135a66eeb0426
wwwyssa <grisha.maryin@gmail.com>
2025-11-13 21:40:48 +0300

    add smth

--- bot/botMax.py
+++ bot/botMax.py
@@ -1,92 +1,148 @@
 import aiomax
 import time
 
+from aiomax import fsm
+
+from utils import Timer, MapTimer, User, MapUser, Task
+
 bot = aiomax.Bot("f9LHodD0cOIRY0kxaAAsvfhYqEv2ley3x9B2T7Mn6JIxw7Y6i5U8Wu1eMGbWXUNk1menHVRnTDdwyLRUe6mA",
                  default_format="markdown")
 
 
-class Timer:
-    id: int
-    time_start: float
-    time_end: float
-    msg_start: int
-
-    def __init__(self, id: int, time_start: float, msg_id: int = None) -> None:
-        self.id = id
-        self.time_start = time_start
-        self.msg_start = msg_id
-
-    def add_msg_id(self, msg_id) -> None:
-        self.msg_start = msg_id
-
-
-class MapTimer:
-    timers: dict[int: Timer]
-
-    def __init__(self):
-        self.timers = {}
-
-    def add(self, timer: Timer) -> None:
-        self.timers[timer.id] = timer
-
-    def get(self, id: int) -> Timer:
-        return self.timers[id]
-
-    def delete(self, id: int) -> None:
-        del self.timers[id]
 
 
 mapTimer = MapTimer()
-
+mapUser = MapUser()
 
 @bot.on_bot_start()
 @bot.on_command('start')
 async def start(pd: aiomax.BotStartPayload):
+    global mapUser
+    user_name = None
+    if type(pd) == aiomax.BotStartPayload:
+        user_name = pd.user.name
+    else:
+        user_name = pd.sender.name
+    user_id = pd.user_id
+
+    user = User(user_id, user_name)
+    mapUser.add(user)
+
     kbStart = aiomax.buttons.KeyboardBuilder()
-    b = aiomax.buttons.CallbackButton("Start Timer", "start_timer")
+    b = aiomax.buttons.CallbackButton("Создать таймер", "create_timer")
     kbStart.add(b)
     await pd.send("POMODORO BOT", keyboard=kbStart)
 
 
-# Отправляем сообщение с кнопкой при вводе команды /tap
-@bot.on_command('start_timer')
-@bot.on_button_callback('start_timer')
-async def start_timer(ctx: aiomax.CommandContext):
+@bot.on_command('create_timer')
+@bot.on_button_callback('create_timer')
+async def create_timer(ctx: aiomax.CommandContext):
     global mapTimer
 
     if ctx.user_id in mapTimer.timers:
         t = mapTimer.get(ctx.user_id)
         await bot.delete_message(str(t.msg_start))
-
     timer = Timer(ctx.user_id, time.time())
 
     kbTimer = aiomax.buttons.KeyboardBuilder()
-    b = aiomax.buttons.CallbackButton("End Timer", "end_timer")
-    kbTimer.add(b)
+    btn1 = aiomax.buttons.CallbackButton("Запустить таймер", "start_timer")
+    btn2 = aiomax.buttons.CallbackButton("Создать задачу", "create_task")
+    kbTimer.add(btn1)
+    kbTimer.add(btn2)
+
     m = await ctx.message.reply(
-        f"Timer Start at {time.localtime().tm_hour}:{'0' if time.localtime().tm_min < 10 else ''}{time.localtime().tm_min}",
+        f"Таймер создан. Вы можете запустить его или создать задачу.",
         keyboard=kbTimer
     )
     timer.add_msg_id(m.id)
     mapTimer.add(timer)
 
 
-@bot.on_button_callback("end_timer")
+# Отправляем сообщение с кнопкой при вводе команды /start_timer
+@bot.on_command('start_timer')
+@bot.on_button_callback('start_timer')
+async def start_timer(ctx: aiomax.CommandContext):
+    global mapTimer
+
+    kbTimer = aiomax.buttons.KeyboardBuilder()
+    btn1 = aiomax.buttons.CallbackButton("Остановить таймер", "stop_timer")
+    kbTimer.add(btn1)
+
+    timer = mapTimer.get(ctx.user_id)
+    timer.set_time_start(time.time())
+
+    m = await ctx.message.reply(
+        f"Таймер запущен в {time.localtime().tm_hour}:{'0' if time.localtime().tm_min < 10 else ''}{time.localtime().tm_min}",
+        keyboard=kbTimer
+    )
+
+
+@bot.on_button_callback('create_task')
+async def create_task(message: aiomax.Message, cursor: fsm.FSMCursor):
+    await message.send("Введите название задачи")
+    cursor.change_state('name_of_task')
+
+
+@bot.on_message(aiomax.filters.state('name_of_task'))
+async def write_name(message: aiomax.Message, cursor: fsm.FSMCursor):
+    await message.reply("Введите ценность задачи (1-10)")
+
+    cursor.change_state('enter_value')
+    cursor.change_data({'name_of_task': message.content})
+
+@bot.on_message(aiomax.filters.state('enter_value'))
+async def write_name(message: aiomax.Message, cursor: fsm.FSMCursor):
+    global mapUser
+    name_of_task = cursor.get_data()['name_of_task']
+    if (not message.content.isdigit()) or (int(message.content) < 1) or (int(message.content) > 10):
+        await message.reply("Ошибка ввода. Введите ценность задачи (1-10)")
+        return
+    value_of_task = int(message.content)
+    task = Task(name_of_task, value_of_task)
+
+    user = mapUser.get(message.user_id)
+    user.add_task(task)
+
+
+    kbTimer = aiomax.buttons.KeyboardBuilder()
+    btn1 = aiomax.buttons.CallbackButton("Запустить таймер", "start_timer")
+    kbTimer.add(btn1)
+    await message.reply(f"Задача {name_of_task} создана, ценность {value_of_task}", keyboard=kbTimer)
+
+    cursor.clear()
+
+
+@bot.on_button_callback("stop_timer")
 async def end_timer(ctx: aiomax.CommandContext):
     global mapTimer
     timer = mapTimer.get(ctx.user_id)
     await bot.delete_message(str(timer.msg_start))
     timer.time_end = time.time()
-
     kbEnd = aiomax.buttons.KeyboardBuilder()
-    b = aiomax.buttons.CallbackButton("Start Timer", "start_timer")
-    kbEnd.add(b)
+    btn1 = aiomax.buttons.CallbackButton("Запустить таймер", "start_timer")
+    btn2 = aiomax.buttons.CallbackButton("Закончить задачу", "end_task")
+    kbEnd.add(btn1)
+    kbEnd.add(btn2)
     t = time.localtime(time.time() - timer.time_start)
-    mapTimer.delete(ctx.user_id)
     await ctx.reply(
-        f"Concentrate Time: {'0' if t.tm_min < 10 else ''}{t.tm_min}:{'0' if t.tm_sec < 10 else ''}{t.tm_sec}\nTimer End at {time.localtime().tm_hour}:{'0' if time.localtime().tm_min < 10 else ''}{time.localtime().tm_min}",
+        f"Время работы: {'0' if t.tm_min < 10 else ''}{t.tm_min}:{'0' if t.tm_sec < 10 else ''}{t.tm_sec}\nТаймер остановился в {time.localtime().tm_hour}:{'0' if time.localtime().tm_min < 10 else ''}{time.localtime().tm_min}",
         keyboard=kbEnd
     )
+
+@bot.on_button_callback('end_task')
+async def end_task(ctx: aiomax.CommandContext):
+    global mapTimer, mapUser
+    timer = mapTimer.get(ctx.user_id)
+    await bot.delete_message(str(timer.msg_start))
+    mapTimer.delete(ctx.user_id)
+    keyboard = aiomax.buttons.KeyboardBuilder()
+    btn1 = aiomax.buttons.CallbackButton("Создать таймер", "create_timer")
+    keyboard.add(btn1)
+    user = mapUser.get(ctx.user_id)
+
+    print(user.get_tasks())
+
+    await ctx.reply("Задача завершена. Вы можете создать новый таймер и задачу", keyboard=keyboard)
 
 
 if __name__ == "__main__":
---
+++ bot/utils.py
@@ -0,0 +1,96 @@
+class Task:
+    name: str
+    value: int
+
+    def __init__(self, name: str, value: int) -> None:
+        self.name = name
+        self.value = value
+
+
+class Timer:
+    id: int
+    time_start: float
+    time_end: float
+    msg_start: int
+    task: Task
+
+    def __init__(self, id: int, time_start: float, msg_id: int = None) -> None:
+        self.id = id
+        self.time_start = time_start
+        self.msg_start = msg_id
+        self.task = None
+
+
+
+    def set_time_start(self, time_start: float) -> None:
+        self.time_start = time_start
+
+    def add_msg_id(self, msg_id) -> None:
+        self.msg_start = msg_id
+
+    def add_task(self, task: Task) -> None:
+        self.task = task
+
+
+class MapTimer:
+    timers: dict[int: Timer]
+
+    def __init__(self):
+        self.timers = {}
+
+    def add(self, timer: Timer) -> None:
+        self.timers[timer.id] = timer
+
+    def get(self, id: int) -> Timer:
+        return self.timers[id]
+
+    def delete(self, id: int) -> None:
+        del self.timers[id]
+
+class User:
+    id: int
+    name: str
+    points: int
+    tasks: list[Task]
+    time_spent: float
+
+    def __init__(self, id:int,  name: str) -> None:
+        self.id = id
+        self.name = name
+        self.points = 0
+        self.tasks = []
+        self.time_spent = 0.0
+
+    def add_task(self, task: Task) -> None:
+        self.tasks.append(task)
+
+    def add_points(self, points: int) -> None:
+        self.points += points
+
+    def add_time_spent(self, time_spent: float) -> None:
+        self.time_spent += time_spent
+
+    def get_tasks(self) -> list[Task]:
+        return self.tasks
+
+    def get_points(self) -> int:
+        return self.points
+
+    def get_time_spent(self) -> float:
+        return self.time_spent
+
+
+class MapUser:
+    users: dict[int: User]
+
+    def __init__(self):
+        self.users = {}
+
+    def add(self, user: User) -> None:
+        self.users[user.id] = user
+
+    def get(self, id: int) -> User:
+        return self.users[id]
+
+    def delete(self, id: int) -> None:
+        del self.users[id]
